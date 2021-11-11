8c91cdcac8cc258f7f01a86de861f3160638ade5
Brian Diaz <bidiaz101@gmail.com>
2021-11-11 08:51:07 -0600

    Completed in JS

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,5 +1,14 @@
-function selectionSortRecursive(arr) {
+function selectionSortRecursive(arr, sorted = []) {
   // type your code here
+  if(arr.length === 0) return sorted
+
+  const min = Math.min(...arr)
+  const index = arr.indexOf(min)
+
+  sorted.push(min)
+  arr.splice(index, 1)
+
+  return selectionSortRecursive(arr, sorted)
 }
 
 if (require.main === module) {
@@ -7,10 +16,39 @@ if (require.main === module) {
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
 
-  console.log("");
+  console.log("Expecting: [1, 2, 3, 4, 5, 6, 7]");
+  console.log("=>", selectionSortRecursive([7, 6, 5, 4, 3, 2, 1]))
+
+  console.log("Expecting: [1, 2, 4, 6, 7, 9, 10, 13, 13]]")
+  console.log("=>", selectionSortRecursive([9, 7, 4, 2, 1, 13, 10, 6, 13]))
+
+  const startTime = new Date()
+
+  const longInput = []
+
+  for(let i = 0; i < 100; i++){
+    longInput.push(Math.floor(Math.random() * 10))
+  }
+
+  for(let i = 0; i < 1000; i++) {
+    selectionSortRecursive([3, 2, 1])
+    selectionSortRecursive(longInput)
+  }
+
+  const averageRuntime = (new Date() - startTime) / 2000
+
+  console.log(averageRuntime)
 }
 
 module.exports = selectionSortRecursive;
 
 // Please add your pseudocode to this file
+
+// if the array is empty return the sorted array
+// find the minimum
+// find the minimum index
+// add the minimum to the end of the sorted array
+// remove the minimum from original array
+// pass the original array and the sorted array to the recursive call
+
 // And a written explanation of your solution
